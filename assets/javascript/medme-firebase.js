af3cd3ea00a6a492e41cf77f9245979fcc98f517
dirk wiggins <dirk@echo19.com>
2018-03-28 01:32:21 -0400

    reworked the user flow, combining elements

--- assets/javascript/medme-firebase.js
+++ assets/javascript/medme-firebase.js
@@ -12,19 +12,8 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var auth = firebase.auth();
 
-function checkLoginStatus(createAnon = false) {
-    console.log("createAnon: " + createAnon);
-    console.log(auth)
-    var user = auth.currentUser;
-    console.log(user);
-    if (user) {
-        // User is signed in.
-    } else {
-        // No user is signed in.
-        // if (createAnon === "create") {
-        //     createAnonUser("tempDrug")
-        // }
-    }
+if (page !== "About") {
+    $(".menu-about-link").html("<a href='#about'>ABOUT</a>");
 }
 
 $(document).ready(function () {
@@ -34,14 +23,15 @@ $(document).ready(function () {
         if (user) {
             // do things when logged in, like show the logout button and hide the login button
             console.log("I am: " + user.email);
+            $(".menu-signinout-link").html("<a id='btn-sign-out'>SIGN OUT</a>");
+            $(".menu-profile-link").html("<a href='myprofile.html'>MY PROFILE</a>");
         } else {
-
             // do things when logged out, like show the login button and hide the logout button
             console.log("I am: no one");
+            $(".menu-signinout-link").html("<a href='signIn.html'>SIGN IN</a>");
         }
     });
 });
-
 
 // auto complete states
 // $(function () {
@@ -50,30 +40,6 @@ $(document).ready(function () {
 //         source: availableStates
 //     });
 // });
-
-
-function createAnonUser(initialDrug) {
-    // sign in an anonymous user
-    auth.signInAnonymously()
-        // then add a dummy record to that user (with the initial drug)
-        .then(function (result) {
-            database.ref('users/' + auth.currentUser.uid).set({
-                firstName: "",
-                lastName: "",
-                address: "",
-                city: "",
-                state: "",
-                zip: "",
-                email: "",
-                phone: "",
-                lat: "",
-                lng: "",
-                initialDrug: initialDrug,
-                timeZone: "",
-                termsAccepted: "",
-            });
-        });
-}
 
 function addDrug(drugName, drugDosage, drugFrequency, RxCUI) {
     // add a new drug to the user
@@ -85,45 +51,32 @@ function addDrug(drugName, drugDosage, drugFrequency, RxCUI) {
     });
     if (drugFrequency === "As Needed") {
         $(".alert-area").html("<div class='alert alert-success fade in alert-dismissible'><a href='#' class='close' data-dismiss='alert'>&times;</a>" + drugName + " was successfully added.</div>");
-    }
-    else {
+    } else {
         $(".alert-area").html("<div class='alert alert-success fade in alert-dismissible'><a href='#' class='close' data-dismiss='alert'>&times;</a>" + drugName + " was successfully added. You will receive text messages to help you remember when to take your medicine.</div>");
     }
-
 }
 
-function convertAnonToCredentialedUser(email, password, firstName, lastName, address, city, state, zip, phone, termsAccepted) {
-    // create the email and password credential, to upgrade the anonymous user
-    var credential = firebase.auth.EmailAuthProvider.credential(email, password);
-    // link the credential to the currently signed in (anonymous) user
-    auth.currentUser.linkWithCredential(credential)
-        // then update the users profile record with the information they filled out
-        .then(function (user) {
-            database.ref('users/').child(user.uid).update({
-                firstName: firstName,
-                lastName: lastName,
-                address: address,
-                city: city,
-                state: state,
-                zip: zip,
-                email: email,
-                phone: phone,
-                termsAccepted: Date.now(),
-            });
-            $(".alert-area").html("<div class='alert alert-success fade in alert-dismissible'><a href='#' class='close' data-dismiss='alert'>&times;</a>New user account successfully added.</div>");
-        }, function (error) {
-            $(".alert-area").html("<div class='alert alert-danger fade in alert-dismissible'><a href='#' class='close' data-dismiss='alert'>&times;</a>" + error.message + "<br /><br />Try again or <a href='login.html' class='alert-link'>login with an existing account here</a></div>");
-            console.log("Error upgrading anonymous account", error);
+function createAccountAndLogin(email, password, firstName, lastName, address, city, state, zip, phone, termsAccepted) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
+        console.log('everything went fine');
+        console.log('user object:' + user);
+        database.ref('users/' + auth.currentUser.uid).set({
+            firstName: firstName,
+            lastName: lastName,
+            address: address,
+            city: city,
+            state: state,
+            zip: zip,
+            email: email,
+            phone: phone,
+            termsAccepted: Date.now(),
         });
+        $(".alert-area").html("<div class='alert alert-success fade in alert-dismissible'><a href='#' class='close' data-dismiss='alert'>&times;</a>New user account successfully added.</div>");
+        $(location).attr('href','myprofile.html');
+    }).catch(function(error) {
+        $(".alert-area").html("<div class='alert alert-danger fade in alert-dismissible'><a href='#' class='close' data-dismiss='alert'>&times;</a>" + error.message + "<br /><br />Try again or <a href='signin.html#signin' class='alert-link'>login with an existing account here</a></div>");
+    });
 }
-
-// watch for teaser drug request
-$("#btn-submit-initial-drug").click(function () {
-    // get the teaser drug from the user input
-    var initialDrug = $("#initial-drug-name").val();
-    // call function to create anonymous user with teaser drug
-    createAnonUser(initialDrug);
-});
 
 // watch for add drug request
 // $("#btn-add-drug").click(function () {
@@ -136,7 +89,7 @@ $("#btn-submit-initial-drug").click(function () {
 //     addDrug(drugName, drugDosage, drugFrequency, RxCUI);
 // });
 
-//watch for profile update, at which time we transfer anonymous account to a credentialed account
+//watch for profile submit, at which time we generate a credentialed account
 $(document).on('submit', '#registration', function (event) {
     console.log("button");
     event.preventDefault();
@@ -153,61 +106,43 @@ $(document).on('submit', '#registration', function (event) {
     var termsAccepted = $("#terms-accepted").val();
     // convert the anonymous account to a credentialed account and fill user profile with input data
     console.log(email, password, firstName, lastName, address, city, state, zip, phone, termsAccepted);
-    convertAnonToCredentialedUser(email, password, firstName, lastName, address, city, state, zip, phone, termsAccepted);
+    createAccountAndLogin(email, password, firstName, lastName, address, city, state, zip, phone, termsAccepted);
 });
 
-
 // watch for logout
-$("#btn-logout").click(function () {
+$(document).on('click', '#btn-sign-out', function (event) {
     // logout
     auth.signOut();
+    $(location).attr('href','index.html');
 });
 
 // watch for sign in of existing credentialed user (for return to their profile)
-$("#btn-login-with-credentials").click(function () {
+$(document).on('submit', '#sign-in', function (event) {
+    event.preventDefault();
     // get credentials from user input
     var email = $("#login-email").val();
     var password = $("#login-password").val();
-    // lof the user in
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-    });
-});
-
-$("#btn-list-user-drugs").click(function () {
-    console.log("btn-click");
-    // Loop through users in order with the forEach() method. The callback
-    // provided to forEach() will be called synchronously with a DataSnapshot
-    // for each child:
-    var query = database.ref('users/' + auth.currentUser.uid + '/rx').orderByKey();
-    query.once("value")
-        .then(function (snapshot) {
-            snapshot.forEach(function (childSnapshot) {
-                // childData is the actual contents of the child
-                var childData = childSnapshot.val();
-                var drugRow = '<tr><td>' + childData.name + '</td><td>' + childData.dosage + '</td><td>' + childData.frequency + '</td><td>' + childData.frequency + '</td><td><a class="btn btn-small btn-danger delete" id="' + childData.RxCUI + '">Delete</a></td></tr>';
-                $("#drugtable").append(drugRow)
-                console.log(childData.name);
-                console.log(childData.dosage);
-                console.log(childData.frequency);
-                console.log(childData.RxCUI);
-            });
+    // log the user in
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(function() {
+            $(location).attr('href','myprofile.html');
+        })
+        .catch(function (error) {
+            // Handle Errors here.
+            $(".alert-area").html("<div class='alert alert-danger fade in alert-dismissible'><a href='#' class='close' data-dismiss='alert'>&times;</a>" + error.message + "<br />Try again or <a href='reset.html' class='alert-link'>reset your password</a></div>");
         });
 });
 
 $(document).on('click', '.btn-remove-drug', function (event) {
     event.preventDefault();
+    $("#drug-table").html("");
     deleteRef = database.ref('users/' + auth.currentUser.uid + '/rx/' + $(this).attr("id"));
     deleteRef.remove();
 });
 
 $(document).on('click', '#btn-list-drug-interaction', function (event) {
     event.preventDefault();
-    
+
     // Loop through users in order with the forEach() method. The callback
     // provided to forEach() will be called synchronously with a DataSnapshot
     // for each child:
@@ -229,6 +164,7 @@ $(document).on('click', '#btn-list-drug-interaction', function (event) {
                     console.info(data);
                     var interactions = "";
                     $(".alert-area").html("");
+                    var html = "<div class='alert alert-warning fade in alert-dismissible'><a href='#' class='close' data-dismiss='alert'>&times;</a>";
                     // loop through interaction type groups - this is specific to the data format of the NIH API response
                     for (i = 0; i < data.fullInteractionTypeGroup.length; i++) {
                         // loop through interaction types - this is specific to the data format of the NIH API response
@@ -236,9 +172,10 @@ $(document).on('click', '#btn-list-drug-interaction', function (event) {
                             console.log(data.fullInteractionTypeGroup[i].fullInteractionType[j].interactionPair[0].description);
                             console.log(data.fullInteractionTypeGroup[i].fullInteractionType[j].interactionPair[0].severity);
                             interactions += interactions + '<br /> <br />' + data.fullInteractionTypeGroup[i].fullInteractionType[j].interactionPair[0].description;
-                            $(".alert-area").append("<div class='alert alert-warning fade in alert-dismissible'><a href='#' class='close' data-dismiss='alert'>&times;</a>" + data.fullInteractionTypeGroup[i].fullInteractionType[j].interactionPair[0].description + "</div>");
+                            html += data.fullInteractionTypeGroup[i].fullInteractionType[j].interactionPair[0].description + "<br>";
                         }
                     }
+                    $(".alert-area").append(html+"</div>");
                 }
             });
         });
